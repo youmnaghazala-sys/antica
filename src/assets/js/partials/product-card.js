5c65a8327542a458007db89201109e321a4c213e
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-10 10:42:57 +0200

    update: product card

--- src/assets/js/partials/product-card.js
+++ src/assets/js/partials/product-card.js
@@ -265,7 +265,7 @@ class ProductCard extends HTMLElement {
             : ``}
 
 
-          ${!this.hideAddBtn ?
+          ${!this.hideAddBtn && !this.horizontal ?
             `<div class="s-product-card-content-footer gap-2">
               <salla-add-product-button fill="outline" width="wide"
                 product-id="${this.product.id}"
@@ -293,6 +293,32 @@ class ProductCard extends HTMLElement {
             </div>`
             : ``}
         </div>
+        ${!this.hideAddBtn && this.horizontal ?
+          `<div class="s-product-card-content-footer gap-2">
+            <salla-add-product-button fill="outline" width="wide"
+              product-id="${this.product.id}"
+              product-status="${this.product.status}"
+              product-type="${this.product.type}">
+              ${this.product.status == 'sale' ? 
+                  `<i class="text-base sicon-${ this.product.type == 'booking' ? 'calendar-time' : 'shopping-bag'}"></i>` : ``
+                }
+              <span>${this.product.add_to_cart_label ? this.product.add_to_cart_label : this.getAddButtonLabel() }</span>
+            </salla-add-product-button>
+
+            <salla-button 
+              shape="icon" 
+              fill="outline" 
+              color="light" 
+              id="card-wishlist-btn-${this.product.id}-horizontal"
+              aria-label="Add or remove to wishlist"
+              class="s-product-card-wishlist-btn animated ${this.isInWishlist ? 's-product-card-wishlist-added pulse-anime' : 'not-added un-favorited'}"
+              onclick="salla.wishlist.toggle(${this.product.id})"
+              data-id="${this.product.id}">
+              <i class="sicon-heart"></i> 
+            </salla-button>
+              
+          </div>`
+          : ``}
       `
 
       this.querySelectorAll('[name="donating_amount"]').forEach((element)=>{
