c24479d7c5ee2fa22b323d1512b91ab36000a959
youseffathy <yf1102003@gmail.com>
2025-04-30 14:31:04 +0300

    add product card description multi line option

--- src/assets/js/partials/product-card.js
+++ src/assets/js/partials/product-card.js
@@ -25,7 +25,7 @@ class ProductCard extends HTMLElement {
       this.getProps()
 
 	  this.source = salla.config.get("page.slug");
-      // If the card is in the landing page, hide the add button and show the quantity
+    // If the card is in the landing page, hide the add button and show the quantity
 	  if (this.source == "landing-page") {
 	  	this.hideAddBtn = true;
 	  	this.showQuantity = window.showQuantity;
